eb979375ee7dcbb51d898607207e59036a1c646f
Anna Laura Castro Sousa <117537124+annalaura2@users.noreply.github.com>
2023-08-15 22:58:12 -0300

    Create exercicio14.c

---
+++ exercicio14.c
@@ -0,0 +1,43 @@
+/*14. Faça um programa que leia um vetor de 10 posições e verifique se existem valores iguais
+e os escreva na tela.*/
+
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+    int main()
+    {
+
+        int num[10], i = 0, k = 0, aux;
+
+        printf("Digite 10 numeros:\n");
+
+        for (i = 0; i < 10; i++)
+        {
+
+            printf("[%i]", i + 1);
+
+            scanf("%d", &num[i]);
+        }
+
+        printf("\n\nNumeros repetidos:\n");
+
+        for (i = 0; i < 10; i++)
+        {
+
+            for (k = i + 1; k < 10; k++)
+            {
+
+                if (num[k] == num[i])
+                {
+
+                    aux = num[i];
+
+                    printf("%d\n", aux);
+                }
+            }
+        }
+
+        return 0;
+    }
